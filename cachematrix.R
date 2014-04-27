ee698099a3dc3d3fa6c87206eacd27dfdf460e9e
Patric Weitzel <patric.l.w@gmail.com>
2014-04-27 12:21:10 +0200

    Implemented necessary functions for the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions provide a way to cache the computed inversion of
+## a matrix and thereby save computation time if the inversion for
+## the same matrix in needed again. The used matrix must be invertible.
 
-## Write a short comment describing this function
 
+
+## Creates a list of functions for the provided matrix which make the
+## caching of the inversion of the matrix possible.
+## In an object oriented view, the matrix is wrapped in a new obejct
+## with a property for the matrix and the cached inversion.
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## Initilize the environement for the returned functions.
+        currentMatrix <- x
+        cachedInversion <- NULL
+        ## Function to access the provided value
+        getMatrix <- function(){
+                currentMatrix
+        }
+        ## This function sets the parameter as the new value for the matrix. This also invalidates the cached
+        ## value for the inversion.
+        setMatrix <- function(newMatrix){
+                currentMatrix <<- newMatrix
+                cachedInversion <<- NULL
+        }
+        getCachedInversion <- function(){
+                cachedInversion
+        }
+        setCachedInversion <- function(inversion){
+                cachedInversion <<- inversion
+        }
+        list(getMatrix=getMatrix,setMatrix=setMatrix,getCachedInversion=getCachedInversion,setCachedInversion=setCachedInversion)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the cached in version of the provided cacheMatrix if the cached
+## inversion is present. Otherwise it computes the the inversion of the
+## current matrix by calling the solve function, puts the result in the
+## cache of the cacheMatrix and returns the result.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inversion <- x$getCachedInversion()
+        if(is.null(inversion)){
+                currentMatrix <- x$getMatrix()
+                inversion <- solve(currentMatrix,...)
+                x$setCachedInversion(inversion)
+        }else{
+                message("Returning cached data")
+        }
+        inversion
 }
